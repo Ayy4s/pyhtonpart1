79fbfcb512358211035c60c83163a2e81d028a31
Ibrahim Ozcan <112540536+Ayy4s@users.noreply.github.com>
2023-05-14 17:19:50 +0300

    Add files via upload

---
+++ Variable 1.py
@@ -0,0 +1,14 @@
+a = "Laura and tom went ON a long walk."
+
+a = a.replace("ON", "on")
+
+a = a.replace("tom", "Tom")
+
+a = a.replace("long", "LONG")
+
+a = a.replace("a", "o")
+
+print(a)
+
+words = a.split()
+print(words)

b7b2a1b77a60331d4f4964262b8ce4fa70638c90
Ibrahim Ozcan <112540536+Ayy4s@users.noreply.github.com>
2023-05-14 19:31:35 +0300

    Add files via upload

---
+++ Understanding Data Types.py
@@ -0,0 +1,20 @@
+integer_var = 10
+float_var = 3.14
+string_var = "Hello, world!"
+boolean_var = True
+
+print(type(integer_var))
+print(type(float_var))
+print(type(string_var))
+print(type(boolean_var))
+
+result1 = integer_var + 5
+print(result1)
+
+
+result2 = string_var + str(float_var)
+print(result2)
+
+
+result3 = string_var == boolean_var
+print(result3)  

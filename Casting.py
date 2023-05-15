11952c86c9eb055ee6987cbb9586273f6d882441
Ibrahim Ozcan <112540536+Ayy4s@users.noreply.github.com>
2023-05-15 10:13:53 +0300

    Add files via upload

---
+++ Casting.py
@@ -0,0 +1,13 @@
+
+float_num = 3.14
+
+
+int_num = int(float_num)
+
+
+str_num = str(int_num)
+
+
+print("Original float_num:", float_num)
+print("Casting to int:", int_num)
+print("Casting to str:", str_num)

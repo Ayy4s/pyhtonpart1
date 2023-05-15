68897a909dbea5536bde946be6a299c9baa01e1a
Ibrahim Ozcan <112540536+Ayy4s@users.noreply.github.com>
2023-05-15 13:23:30 +0300

    Add files via upload

---
+++ String Manipulation.py
@@ -0,0 +1,22 @@
+
+name = "Hello, World!"
+
+
+first_character = name[0]
+last_character = name[-1]
+
+
+middle_part = name[7:12]
+
+
+new_string = "Goodbye" + name
+
+
+length_of_string = len(name)
+
+
+print("First character:", first_character)
+print("Last character:", last_character)
+print("Middle part:", middle_part)
+print("Concatenated string:", new_string)
+print("Length of the string:", length_of_string)

e9bdec45ab2857365613b278e02058ea880804c6
Ibrahim Ozcan <112540536+Ayy4s@users.noreply.github.com>
2023-05-22 10:44:55 +0300

    Add files via upload

---
+++ Operators 8.py
@@ -0,0 +1,25 @@
+
+a = 3
+b = 4
+sum_ab = a + b
+print("The sum of a and b is:", sum_ab)
+
+
+x = 7
+y = 2
+diff_xy = x - y
+print("The difference of x and y is:", diff_xy)
+
+
+p = 5
+q = 3
+product_pq = p * q
+print("The product of p and q is:", product_pq)
+
+
+m = 8
+n = 2
+quotient_mn = m // n
+remainder_mn = m % n
+print("The quotient of m divided by n is:", quotient_mn)
+print("The remainder of m divided by n is:", remainder_mn)

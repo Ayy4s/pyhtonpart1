78b527620d1f8b64e4cff76aa41d7fbd783c77f1
Ibrahim Ozcan <112540536+Ayy4s@users.noreply.github.com>
2023-05-23 13:31:18 +0300

    Add files via upload

---
+++ Operators  9.py
@@ -0,0 +1,26 @@
+a = 3
+b = 4
+result = b // a
+print(result)
+
+
+a = 3
+b = 4
+result = b % a
+print(result)
+
+numbers = [3, 5, 7, 9, 11]
+result = sum(numbers)
+print(result)
+
+
+grades = [83, 85, 72, 63, 95]
+average = sum(grades) / len(grades)
+print(average)
+
+
+prices = [5, 10, 15, 20, 25]
+highest = max(prices)
+lowest = min(prices)
+print("Highest price:", highest)
+print("Lowest price:", lowest)

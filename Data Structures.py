619b4c7c65125c91ffae57c050f5016dc3722c0d
Ibrahim Ozcan <112540536+Ayy4s@users.noreply.github.com>
2023-06-16 19:08:29 +0300

    Add files via upload

---
+++ Data Structures.py
@@ -0,0 +1,10 @@
+Length of list1: 4
+Length of tuple1: 4
+Length of set1: 4
+Length of dict1: 4
+First element of list1: lion
+First element of tuple1: lion
+Value of 'lion' key in dict1: land
+Updated list1: ['lion', 'dog', 'fish', 'monkey']
+Updated tuple1: ('lion', 'monkey', 'dog', 'fish')
+Updated dict1: {'lion': 'land', 'monkey': 'land', 'dog': 'land', 'fish': 'fish'}

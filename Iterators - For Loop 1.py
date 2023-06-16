f552153420aca83f051de127131d5d0540bc2c99
Ibrahim Ozcan <112540536+Ayy4s@users.noreply.github.com>
2023-06-16 18:49:20 +0300

    Add files via upload

--- Iterators - For Loop 1.py
+++ Iterators - For Loop 1.py
@@ -1,4 +1,6 @@
-for i in range(1, 6):
-    if i % 2 == 0:
-        continue
-    print("*" * i)
+todo = ["exercise1", "exercise2", "exercise3", "COFFEE BREAK", "exercise4", "exercise5", "exercise6"]
+
+for item in todo:
+    if item.lower() == "coffee break":
+        print("It's time for a coffee break!")
+        break
